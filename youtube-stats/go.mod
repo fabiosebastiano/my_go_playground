98397686bfd5c1b490ba6d4153a0a6eb54312863
Fabio Sebastiano <sebastiano.fabio@gmail.com>
2019-07-11 12:51:35 +0200

    fixati tutti i problemi...

--- youtube-stats/go.mod
+++ youtube-stats/go.mod
@@ -2,8 +2,4 @@ module github.com/fabiosebastiano/my_go_playground/youtube-stats
 
 go 1.12
 
-require (
-	github.com/gorilla/websocket v1.4.0 // indirect
-	github.com/fabiosebastiano/my_go_playground/youtube-stats/websocket v0.0.3
-	github.com/fabiosebastiano/my_go_playground/youtube-stats/youtube v0.0.3
-)
+require github.com/gorilla/websocket v1.4.0
--- youtube-stats/websocket/websocket.go
+++ youtube-stats/websocket/websocket.go
@@ -23,11 +23,16 @@ var upgrader = websocket.Upgrader{
 func Upgrade(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
 	//permette ad host diversi dal nostro di connettersi al nostro websocket server
 	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
-
+	
+	r.Header.Set("Connection", "upgrade")
+	r.Header.Set("Upgrade", "websocket")
+	r.Header.Set("Sec-Websocket-Version", "13")
+	r.Header.Set("Sec-Websocket-Key", "LK2Qjb+GReKaqQrn4zEvsA==")
+	
 	//creiamo la connessione websocket
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Println(err)
+		log.Println("!! errore durante upgrade!!! ", err)
 		return ws, err
 	}
 
@@ -48,13 +53,14 @@ func Writer(conn *websocket.Conn) {
 			//chiamo il servizio interno
 			items, err := youtube.GetSubscribers()
 			if err != nil {
-				fmt.Printf(err)
+				fmt.Println(err)
 			}
 			//se non ci sono errori, marshal dell'oggetto in jsonString
 			jsonString, err := json.Marshal(items)
 			if err != nil {
-				fmt.Printf(err)
+				fmt.Println(err)
 			}
+			
 			// and finally we write this JSON string to our WebSocket
 			// connection and record any errors if there has been any
 			if err := conn.WriteMessage(websocket.TextMessage, []byte(jsonString)); err != nil {
